42b46bc81f75e96cdcd2db242a083aedb8825732
Ryan Bennink <benninkrs@ornl.gov>
2022-03-08 23:21:09 -0500

    Added support for arbitrary dimension arrays. Improved documentation.

--- src/DiagonalIndexing.jl
+++ src/DiagonalIndexing.jl
@@ -4,8 +4,6 @@ Provides a convenient way of selecting diagonal elements of arrays.
 module DiagonalIndexing
 export diagonal
 
-# TODO:  Generalize to multidimensional arrays
-
 
 import Base: getindex, to_indices, IndexStyle, index_ndims, checkbounds, size, length
 
@@ -17,8 +15,8 @@ import Base: getindex, to_indices, IndexStyle, index_ndims, checkbounds, size, l
 # we cannot use a generator; the collection must be <: AbstractArray.
 # Thus, we define a type of lazy array of CartesianIndex's.
 
-struct DiagCartesianIndices <: AbstractArray{Int64,1} 
-	ax::NTuple{2, Base.OneTo{Int64}}
+struct DiagCartesianIndices{T} <: AbstractArray{Int64,1} 
+	ax::T
 end
 
 
@@ -30,14 +28,14 @@ end
 
 # Define additional indexing properties
 IndexStyle(::DiagCartesianIndices) = IndexLinear()
-length(ind::DiagCartesianIndices) = min(length(ind.ax[1]), length(ind.ax[2]))
+length(ind::DiagCartesianIndices) = minimum(length.(ind.ax))
 size(ind::DiagCartesianIndices) = (length(ind),)
 
 # By construction, DiagCartesianIndices are always inbounds
 checkbounds(A::AbstractArray, ::DiagCartesianIndices) = nothing
 
 # The returned CartesianIndex occupies 2 index positions
-@inline index_ndims(::DiagCartesianIndices) = (true, true)
+@inline index_ndims(::DiagCartesianIndices{<:NTuple{N}}) where N = ntuple(i->true, Val(N))
 
 
 
@@ -54,6 +52,11 @@ end
 """
 Singleton object that, when used as an index, selects the diagonal
 elements of an array.
+
+`A[diagonal]` selects A[1,1,...], A[2,2,...], ..., A[k,k,...] where
+k = minimum(size(A)).
+
+# Examples
 
 ```
 julia> using DiagonalIndexing
@@ -75,10 +78,10 @@ julia> A[diagonal] = [-10, -50, -90];
 
 julia> A
 4×3 Matrix{Int64}:
-  -10    2    3
-	 4  -50    6
-	 7    8  -90
-	10   11   12
+ -10    2    3
+   4  -50    6
+   7    8  -90
+  10   11   12
 ```
 """
 const diagonal = DiagonalIndex()
@@ -86,7 +89,11 @@ const diagonal = DiagonalIndex()
 
 # Implement the indexing behavior
 
-to_indices(A::AbstractMatrix, d::Tuple{DiagonalIndex}) = _to_indices(IndexStyle(A), A, d)
+to_indices(A::AbstractArray, d::Tuple{DiagonalIndex}) = _to_indices(IndexStyle(A), A, d)
+function to_indices(A::AbstractVector, d::Tuple{DiagonalIndex})
+	(length(A) > 0 ? [firstindex(A)] : [], )
+end 
+
 
 function _to_indices(::IndexLinear, A::AbstractMatrix, ::Tuple{DiagonalIndex})
 	(m, n) = size(A)
@@ -94,7 +101,21 @@ function _to_indices(::IndexLinear, A::AbstractMatrix, ::Tuple{DiagonalIndex})
 end
 
 
-function _to_indices(::IndexCartesian, A::AbstractMatrix, ::Tuple{DiagonalIndex})
+function _to_indices(::IndexLinear, A::AbstractArray, ::Tuple{DiagonalIndex})
+	s = size(A)
+	step = 1
+	stride = 1
+	k = s[1]
+	for i = 2:ndims(A)
+		stride *= s[i-1]
+		step += stride
+		k = min(k, s[i])
+	end
+	(1:step:1+step*(k-1),)
+end
+
+
+function _to_indices(::IndexCartesian, A::AbstractArray, ::Tuple{DiagonalIndex})
 	(DiagCartesianIndices(axes(A)),)
 end
 
