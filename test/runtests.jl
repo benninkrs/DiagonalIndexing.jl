c062fba3a14234c24edbb009cc35c5cf2e82b0a5
Ryan Bennink <benninkrs@ornl.gov>
2025-03-18 12:09:35 -0400

    * Fixed bug in calculation of range when diagonal dimension > number of axes * Renamed diagnl to diagonal and implemented as a function * Renamed Diagnl to DiagIndex and removed support for "Any" dimension * Changed dispatch on Diagn{Any} to typeof(diagonal) * Updated README * Version 0.8.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,30 +4,30 @@ using DiagonalIndexing
 # test vectors
 # ... get
 x = [0.1, 0.2, 0.3]
-@test x[diagnl] == [0.1, 0.2, 0.3]
+@test x[diagonal] == [0.1, 0.2, 0.3]
 
 
 # test matrices
 # ... having linear indexing
 A = [1 2 3; 4 5 6; 7 8 9; 10 11 12];
-@test A[diagnl] == [1, 5, 9]
+@test A[diagonal] == [1, 5, 9]
 
-A[diagnl] = [-1, -5, -9]
+A[diagonal] = [-1, -5, -9]
 @test A == [-1 2 3; 4 -5 6; 7 8 -9; 10 11 12]
 
-@test A[Diagnl(1,0)] == [4, 8, 12]
-@test A[Diagnl(0,1)] == [2, 6]
+@test A[diagonal(1,0)] == [4, 8, 12]
+@test A[diagonal(0,1)] == [2, 6]
 
 # ... having Cartesian indexing
 B = A'
-@test B[diagnl] == [-1, -5, -9]
+@test B[diagonal] == [-1, -5, -9]
 
 
 # test higher dimenional arrays
 # ...  having linear indexing
 T = collect(reshape(0.1:0.1:6.0, (3,4,5)))
-@test T[diagnl] == [0.1, 1.7, 3.3]
+@test T[diagonal] == [0.1, 1.7, 3.3]
 
 # ... having Cartesian indexing
 S = PermutedDimsArray(T, (3,1,2))
-@test S[diagnl] == [0.1, 1.7, 3.3]
+@test S[diagonal] == [0.1, 1.7, 3.3]
